9288f14b0a2c50dedac4072bcc3848e4535dee77
stiria06 <32118818+stiria06@users.noreply.github.com>
2020-04-23 02:27:16 +0900

    level 2 solved

---
+++ Programmers/level 2/입양 시각 구하기(1).sql
@@ -0,0 +1,5 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59412
+SELECT HOUR, COUNT(*) FROM (
+    SELECT TO_CHAR(DATETIME, 'HH24') AS HOUR FROM ANIMAL_OUTS
+) WHERE (HOUR <= 19 AND HOUR >= 9)
+GROUP BY HOUR ORDER BY HOUR;
